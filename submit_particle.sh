06249a73ee286d1fed21f594f772e0581a287b72
Josue Martinez Moreno <josue.martinezmoreno@anu.edu.au>
2019-09-03 10:04:50 +1000

    PBS handeler

---
+++ submit_particle.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Load modules & global variables
+module load payu
+globalpath=`pwd`
+count=0
+
+# input path
+input_path='/home/156/jm5970/short/mitgcm/input/global_particle_release'
+
+# Loop for every initialization of the particle release:
+for tt in `seq 0 308`
+do
+  # Create folder for running experiment.
+  folder="30d_LADV_part_release_$(printf %05d ${tt%})"
+  mkdir $folder
+  # Modify corresponding files to setup the experiment.
+  sed s-{0}-$(printf %05d ${tt%})-g config_sed.yaml > "$folder/config.yaml"
+  cp ./input/* $folder/.
+  sed s-input_off-30d/30d_slice_chunk_$(printf %05d ${tt%})-g input/data.off > "$folder/data.off" 
+  sed s-{0}-$(printf %05d ${tt%})-g config_sed.yaml > "$folder/config.yaml"
+  sed s-{0}-30d_slice_chunk_$(printf %05d ${tt%})-g input/clear_archive.sh > "$folder/clear_archive.sh"
+
+  cd $folder
+
+  # Run the experiment. 
+  payu run -i 0  
+
+  cd $globalpath
+
+  count=$((count+1))
+  # Sleep for 8 hours so the process can be executed, without over queuing PBS.
+  if [ $count -eq 3 ]
+    then
+      echo $count 
+      sleep 8h
+  fi
+  
+done
+
