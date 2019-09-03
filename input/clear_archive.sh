fb62744842ec4f6e4643424589c29154b9076d8d
Josue Martinez Moreno <josue.martinezmoreno@anu.edu.au>
2019-09-03 10:10:14 +1000

    PBS_cleanup

---
+++ input/clear_archive.sh
@@ -0,0 +1,15 @@
+
+cd archive/output000
+
+rm *.nc
+
+rm *profile*.*ta
+
+archive_path='/g/data/v45/jm5970/particle_release/30d'
+experiment_path={0}
+
+mkdir '$archive_path/$experiment_path'
+
+rsync -qar ./* '$archive_path/$experiment_path'
+
+
