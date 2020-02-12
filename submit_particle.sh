230016658e7dc815c551271b26f56378cbb90411
Josue Martinez Moreno <jm5970@gadi-cpu-clx-0258.gadi.nci.org.au>
2020-02-12 17:42:59 +1100

    2020-02-12 17:42:58: Run 0

--- submit_particle.sh
+++ submit_particle.sh
@@ -14,7 +14,7 @@ particle_grid='flt_global_hex_032deg.bin'
 input_path='/scratch/x77/jm5970/mitgcm/input/global_particle_release'
 
 # Loop for every initialization of the particle release:
-for tt in `seq 0 2`
+for tt in `seq 3 320`
 do
   # Create folder for running experiment.
   folder="30d_LADV_part_release_$(printf %05d ${tt%})"
@@ -30,6 +30,9 @@ do
   
   ln -s $input_path/${particle_grid} $input_path/30d/30d_slice_chunk_$(printf %05d ${tt%})/
 
+  # Initiate payu setup
+  payu setup && payu sweep
+  
   # Run the experiment. 
   payu run -i 0  
 
