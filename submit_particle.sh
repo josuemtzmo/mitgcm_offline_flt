b792339a5a18c5a89b9a0d382a0715a107103fde
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2022-04-19 18:05:10 +1000

    non submitted stuff

--- submit_particle.sh
+++ submit_particle.sh
@@ -7,32 +7,33 @@ module load conda/analysis3-unstable
 globalpath=`pwd`
 count=0
 cc=0
-n=25
-nr=10
+n=30
+nr=729
 particle_grid='flt_global_hex_032deg.bin'
-freq=90
+freq=30
 expt="${freq}d"
+expt="1d"
+walltime=3 #hrs
 
 
 # input path
 input_path='/scratch/x77/jm5970/mitgcm/input/global_particle_release'
 
 # Loop for every initialization of the particle release:
-for tt in `seq 0 $nr`
+for tt in `seq 11 $nr`
 do
   # Create folder for running experiment.
   folder="${expt}/${expt}_LADV_part_release_$(printf %05d ${tt%})"
   mkdir $folder
   # Modify corresponding files to setup the experiment.
-  sed -e "s-{0}-$(printf %05d ${tt%})-g;s-{1}-$expt-g;s-{2}-$(printf %02d $((3*freq/30)))-g" config_sed.yaml > "$folder/config.yaml"
+  sed -e "s-{0}-$(printf %05d ${tt%})-g;s-{1}-$expt-g;s-{2}-$(printf %02d ${walltime})-g" config_sed.yaml > "$folder/config.yaml"
   cp ./input/* $folder/.
   sed -e "s-{0}-${freq}-g" input/data > "$folder/data" 
   sed -e "s-{0}-.-g;s-{1}-$(((freq-1)*86400))-g" input/data.off > "$folder/data.off" 
   sed s-flt_global_hex_10deg.bin-${particle_grid}-g input/data.flt > "$folder/data.flt" 
-  sed -e "s-{0}-30d_slice_chunk_$(printf %05d ${tt%})-g;s-{1}-$expt-g" input/clear_archive.sh > "$folder/clear_archive.sh"
+  sed -e "s-{0}-${expt}_slice_chunk_$(printf %05d ${tt%})-g;s-{1}-$expt-g" input/clear_archive.sh > "$folder/clear_archive.sh"
   cd $folder
   
-#  ln -s $input_path/${particle_grid} $input_path/${expt}/${expt}_30t_chunk_$(printf %05d ${tt%})/
   ln -s $input_path/${particle_grid} $input_path/${expt}/${expt}_slice_chunk_$(printf %05d ${tt%})/
 
   # Initiate payu setup
@@ -40,7 +41,7 @@ do
   payu setup && payu sweep
   
   # Run the experiment. 
-#  payu run -i 0  
+  payu run -i 0  
 
   cd $globalpath
 
@@ -51,7 +52,7 @@ do
     then
       cc=0
       echo "Sleep submission"
-      sleep 1h
+      sleep 2h
   else
      cc=$((cc+1))
   fi
